9eebf9c50d31a3839c9b6357493fa922407ab543
Muhammadjon <20mamarasulov@gmail.com>
2021-04-26 19:42:50 +0900

    Initial Commit

---
+++ InstagramLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramLogin
+//
+//  Created by Muhammadjon Mamarasulov on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
